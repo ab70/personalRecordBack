b076c4b3b478b767aea6487adc5f4d792916d5b3
ab70 <nurulabrar2369@gmail.com>
2023-02-27 22:08:42 +0600

    lop

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use(flash())
 //used to handle cross-site requests
 app.use(cors({
     credentials:true,
-    origin: true,
+    origin: ['https://personal-front.vercel.app'],
     //exposedHeaders:['set-cookie'],
 }));
 
